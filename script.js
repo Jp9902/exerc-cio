9b4423b5d4823d591bfac884781e97c9809081b6
jp290609 <>
2024-09-04 12:56:54 -0300

    aqui são 4 números onde o maior vai ser devolvido

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calcular() {
-    let numero1 = parseInt(document.getElementById("numero").value);
-    let numero2 = parseInt(document.getElementById("numero2").value);
+    let numero1 = parseInt(document.getElementById("N1").value);
+    let numero2 = parseInt(document.getElementById("N2").value);
+    let numero3 = parseInt(document.getElementById("N3").value);
+    let numero4 = parseInt(document.getElementById("N4").value);
 
-    console.log(Math.pow(numero1, numero2))
+
+    console.log(Math.max(numero1,numero2,numero3,numero4)) 
 }
