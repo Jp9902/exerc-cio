10e7297ff96ffdddfb30f788a3d8875daa1a2b23
jp290609 <>
2024-09-04 12:53:19 -0300

    aqui são 2 números

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    let numero1 = parseInt(document.getElementById("numero").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+
+    console.log(Math.pow(numero1, numero2))
+}
